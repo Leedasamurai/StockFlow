4888ae2196d4e17a3178b2f89d3afd8dce4f91d1
Leedasamurai <lesegoleemac@gmail.com>
2024-08-14 15:30:39 +0200

    tailwind set up and darkmode set up

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,70 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+import { color } from "@mui/system";
+
+// mapping out base colors and automatically set them
+// ...to the opposite color when we set them to dark mode.
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+// map different values to the  oppnosite colors
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "300",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+// function to set up colors
+
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+};
+
+// generating themeOject for when its dark themed or light themed
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkThemed = generateThemeObject(colors, shadeMapping, true);
+
+const theme = {
+  light: {
+    ...lightTheme,
+    white: "#ffffff",
+  },
+  dark: {
+    ...darkThemed,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+  },
+};
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +79,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(theme)],
 };
 export default config;
