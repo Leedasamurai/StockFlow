7880a5c35486d6b290c73a54b25c565a2417ede2
Leedasamurai <lesegoleemac@gmail.com>
2024-08-15 17:34:20 +0200

    Navbar set-up complete

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Bell, Menu, Settings, Sun } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -13,13 +14,41 @@ const Navbar = () => {
         >
           <Menu className="w-4 h-4" />
         </button>
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="search"
+            className="pl-10 pr-4 py-2 w-50 md:w-80 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          />
+
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+            <Bell className="text-gray-500" size={20} />
+          </div>
+        </div>
       </div>
-      <div className="relative">
-        <input
-          type="search"
-          placeholder="Start type to search groups & products"
-          className="pl-10 pr-4 py-2 w-50 md:w-80 border-gary-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-        ></input>
+
+      <div className="flex justify-between items-center gap-5">
+        <div className="hidden md:flex justify-between item-center gap-5">
+          <div>
+            <button>
+              <Sun className="cursor-pointer text-gray-500" size={24} />
+            </button>
+          </div>
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24} />
+            <span className="absolute -top-2 -right-2 inline-flex item-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
+              9+
+            </span>
+          </div>
+          <hr className="w-0 h-7 boarder-solid boarder-l boarder-gray-300 mx-3" />
+          <div className="flex item-center gap-3 cursor-ponter">
+            <div className="w-9 h-9">image</div>
+            <span className="font-semibold">king kold</span>
+          </div>
+        </div>
+        <Link href="/settings">
+          <Settings className="cursor-pointer text-gray-500 size={24}" />
+        </Link>
       </div>
     </div>
   );
