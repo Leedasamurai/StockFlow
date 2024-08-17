a29e338c1738810d036441db9955722e9a8f58b0
Leedasamurai <lesegoleemac@gmail.com>
2024-08-17 14:10:41 +0200

    Navbar darkmode completed

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/app/state";
-import { Bell, Menu, Search, Settings, Sun } from "lucide-react";
+import { setIsDarkmode, setIsSidebarCollapsed } from "@/app/state";
+import { Bell, Menu, Search, Settings, Sun, SunMoon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -12,8 +12,14 @@ const Navbar = () => {
     (state) => state.global.isSidebarCollapsed
   );
 
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkmode(!isDarkMode));
   };
 
   return (
@@ -30,7 +36,7 @@ const Navbar = () => {
           <input
             type="search"
             placeholder="search"
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-70 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
 
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
@@ -40,10 +46,14 @@ const Navbar = () => {
       </div>
 
       <div className="flex justify-between items-center gap-5">
-        <div className="hidden md:flex justify-between item-center gap-5">
+        <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <SunMoon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
