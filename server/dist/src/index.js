9c01598563037b00d2195f2d1dabbd5599a94cea
Leedasamurai <lesegoleemac@gmail.com>
2024-08-21 01:44:14 +0200

    expense route created

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -13,6 +13,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const UserRoutes_1 = __importDefault(require("./routes/UserRoutes"));
+const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 /* Configurations */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -26,6 +27,7 @@ app.use((0, cors_1.default)());
 app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
 app.use("/products", productRoutes_1.default); // http://localhost:8000/products
 app.use("/users", UserRoutes_1.default); // http://localhost:8000/users
+app.use("/expenses", expenseRoutes_1.default); // http://localhost:8000/expenses
 /* Server */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
